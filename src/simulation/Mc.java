580ed046b574344643cb2e9bc604c4c6d20d743b
Andrej Beliančin <3210ado@gmail.com>
2019-04-19 23:25:54 +0200

    vykomentovanie Id kvoli generovaniu a spravnej funkcnosti asistentov

--- src/simulation/Mc.java
+++ src/simulation/Mc.java
@@ -6,9 +6,9 @@ public class Mc extends IdList {
 	//meta! userInfo="Generated code: do not modify", tag="begin"
 	public static final int prichodZakaznikaNaZastavku = 1051;
 	public static final int init = 1004;
-	public static final int start = 1052;
+	//public static final int start = 1052;
 	public static final int prichodVozidlaNaZastavku = 1040;
-	public static final int finish = 1055;
+	//public static final int finish = 1055;
 	public static final int presunVozidlaNaDalsiuZastavku = 1044;
 	public static final int nastupVystupZakaznika = 1048;
 	public static final int zakazniciNastupili = 1060;
