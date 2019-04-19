460ddf11a67ec522754ba13f3cb3a7a2ac2ec391
Andrej Beliančin <3210ado@gmail.com>
2019-04-19 23:07:55 +0200

    table columna item- min widt a max width

--- src/GUI/TableColumnItem.java
+++ src/GUI/TableColumnItem.java
@@ -4,17 +4,21 @@ import java.util.function.Function;
 
 public class TableColumnItem<T> {
 
+    public static final double IGNORE_MAX_WIDTH = -1;
+
     public Function<T, String> spracuj_;
-    public String viewName_;
-    public double minWidth_;
+    public String _viewName;
+    public double _minWidth;
+    public double _maxWidth;
 
     public TableColumnItem(Function<T, String> spracuj, String viewName) {
-        this(spracuj, viewName, 100.0);
+        this(spracuj, viewName, 100.0, IGNORE_MAX_WIDTH);
     }
 
-    public TableColumnItem(Function<T, String> spracuj, String viewName, double minWidth) {
+    public TableColumnItem(Function<T, String> spracuj, String viewName, double minWidth, double maxWidth) {
         this.spracuj_ = spracuj;
-        this.viewName_ = viewName;
-        this.minWidth_ = minWidth;
+        this._viewName = viewName;
+        this._minWidth = minWidth;
+        this._maxWidth = maxWidth;
     }
 }
