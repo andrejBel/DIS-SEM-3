b2697beaee856d94417ba8ba74858588667a3615
Andrej Beliančin <3210ado@gmail.com>
2019-04-22 23:32:59 +0200

    akcia nastup cestujuceho a vystup

--- src/instantAssistants/AkciaNastupCestujuceho.java
+++ src/instantAssistants/AkciaNastupCestujuceho.java
@@ -1,5 +1,8 @@
 package instantAssistants;
 
+import Model.Cestujuci;
+import Model.Enumeracie.STAV_CESTUJUCI;
+import Model.Vozidlo;
 import OSPABA.*;
 import simulation.*;
 import agents.*;
@@ -12,6 +15,24 @@ public class AkciaNastupCestujuceho extends Action {
 
 	@Override
 	public void execute(MessageForm message) {
+		Sprava copy = (Sprava) message.createCopy(); // todo ci kopia, skor kopia
+		Vozidlo vozidlo = copy.getVozidlo();
+		Cestujuci cestujuci = copy.getCestujuci();
+		Integer volneDvere = vozidlo.obsadPrvyIndexVolnychDveri();
+		if (volneDvere == null) {
+			throw new RuntimeException("Dvere musia byt volne");
+		}
+
+		if (!vozidlo.jeVolneMiestoVoVozidle()) {
+			throw new RuntimeException("Vo vozidle musi byt miesto");
+		}
+		cestujuci.setIndexNastupnychDveri(volneDvere);
+		cestujuci.setVozidlo(vozidlo);
+		cestujuci.setStavCestujuci(STAV_CESTUJUCI.NASTUPUJE_DO_VOZIDLA);
+		vozidlo.pridajCestujucehoNaNastup(copy);
+		copy.setAddressee(mySim().findAgent(Id.agentNastupuVystupu));
+		copy.setCode(Mc.nastupCestujuceho);
+		myAgent().manager().request(copy);
 	}
 
 	@Override
--- src/instantAssistants/AkciaVystupCestujuceho.java
+++ src/instantAssistants/AkciaVystupCestujuceho.java
@@ -1,5 +1,8 @@
 package instantAssistants;
 
+import Model.Cestujuci;
+import Model.Enumeracie.STAV_CESTUJUCI;
+import Model.Vozidlo;
 import OSPABA.*;
 import simulation.*;
 import agents.*;
@@ -12,6 +15,25 @@ public class AkciaVystupCestujuceho extends Action {
 
 	@Override
 	public void execute(MessageForm message) {
+		Sprava copy = (Sprava) message.createCopy();
+		Vozidlo vozidlo = copy.getVozidlo();
+		Integer indexVolnychDveri = vozidlo.obsadPrvyIndexVolnychDveri();
+		if (indexVolnychDveri == null) {
+			throw new RuntimeException("Dvere musia byt volne!!!");
+		}
+		if (vozidlo.getCelkovyPocetCestujucichVoVozidle() <= 0) {
+			throw new RuntimeException("Vo vozidle musi byt niekto!!!");
+		}
+
+		Cestujuci cestujuci = vozidlo.odstranCestujucehoVoVozidle().getCestujuci();
+		copy.setCestujuci(cestujuci);
+		cestujuci.setStavCestujuci(STAV_CESTUJUCI.VYSTUPUJE_Z_VOZIDLA);
+		cestujuci.setIndexVytupnychDveri(indexVolnychDveri);
+		vozidlo.pridajCestujucehoNaVystup(copy);
+
+		copy.setAddressee(mySim().findAgent(Id.agentNastupuVystupu));
+		copy.setCode(Mc.vystupCestujuceho);
+		myAgent().manager().request(copy);
 	}
 
 	@Override
