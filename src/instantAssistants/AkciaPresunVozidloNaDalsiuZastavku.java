eb133b550ef79e4bf17f97ecd9ac5d81cba405db
Andrej Beliančin <3210ado@gmail.com>
2019-04-22 23:33:16 +0200

    akcia presun na dalsiu zastavku

--- src/instantAssistants/AkciaPresunVozidloNaDalsiuZastavku.java
+++ src/instantAssistants/AkciaPresunVozidloNaDalsiuZastavku.java
@@ -1,5 +1,6 @@
 package instantAssistants;
 
+import Model.Vozidlo;
 import OSPABA.*;
 import simulation.*;
 import agents.*;
@@ -12,6 +13,18 @@ public class AkciaPresunVozidloNaDalsiuZastavku extends Action {
 
 	@Override
 	public void execute(MessageForm message) {
+		Sprava copy = (Sprava) message.createCopy(); // TODO rozhodnutie ci kopia ci nie
+		Vozidlo vozidlo = copy.getVozidlo();
+		if (vozidlo.getPocetNastupujucichCestujucich() != 0) {
+			throw new RuntimeException("Nikto nemoze nastupovat, proces nastupu neukonceny");
+		}
+		if (vozidlo.getPocetVystupujucichCestujucich() != 0) {
+			throw new RuntimeException("Nikto nemoze vystupovat, proces vystupu neukonceny");
+		}
+
+		copy.setAddressee(mySim().findAgent(Id.agentPohybu));
+		copy.setCode(Mc.presunVozidlaNaDalsiuZastavku);
+		myAgent().manager().notice(copy);
 	}
 
 	@Override
