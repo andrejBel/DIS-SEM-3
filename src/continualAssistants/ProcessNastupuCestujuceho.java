29576b96d684e7cb68b17a4a175518cd92473ccc
Andrej Beliančin <3210ado@gmail.com>
2019-04-22 23:32:34 +0200

    Proces nastup a vystup

--- src/continualAssistants/ProcessNastupuCestujuceho.java
+++ src/continualAssistants/ProcessNastupuCestujuceho.java
@@ -1,12 +1,23 @@
 package continualAssistants;
 
+import Model.Cestujuci;
+import Model.Enumeracie.TYP_VOZIDLA;
+import Model.Vozidlo;
 import OSPABA.*;
+import OSPRNG.RNG;
+import OSPRNG.TriangularRNG;
+import OSPRNG.UniformContinuousRNG;
 import simulation.*;
 import agents.*;
 import OSPABA.Process;
 
 //meta! id="221"
 public class ProcessNastupuCestujuceho extends Process {
+
+	private TriangularRNG _triangularRNG = new TriangularRNG(0.6, 1.2, 4.2, SimulaciaDopravy.GENERATOR_NASAD); // todo uncomment
+	//private TriangularRNG _triangularRNG = new TriangularRNG(60.0, 120.0, 420.0, SimulaciaDopravy.GENERATOR_NASAD); // todo uncoment
+	private UniformContinuousRNG _uniformContinuousRNG = new UniformContinuousRNG(6.0, 10.0, SimulaciaDopravy.GENERATOR_NASAD);
+
 	public ProcessNastupuCestujuceho(int id, Simulation mySim, CommonAgent myAgent) {
 		super(id, mySim, myAgent);
 	}
@@ -19,16 +30,30 @@ public class ProcessNastupuCestujuceho extends Process {
 
 	//meta! sender="AgentNastupuVystupu", id="239", type="Notice"
 	public void processKoniecNastupu(MessageForm message) {
+		assistantFinished(message);
 	}
 
 	//meta! sender="AgentNastupuVystupu", id="229", type="Notice"
 	public void processStart(MessageForm message) {
+		Sprava sprava = (Sprava) message;
+		Vozidlo vozidlo = sprava.getVozidlo();
+		Cestujuci cestujuci = sprava.getCestujuci();
+		RNG<Double> generator = getGeneratorNaZakaldeTypu(vozidlo.getTypVozidla());
+
+		double casNastupu = generator.sample();
+		sprava.setCode(Mc.koniecNastupu);
+		cestujuci.setCasZaciatkuNastupovania(mySim().currentTime());
+		cestujuci.setCasKoncaNastupovania(mySim().currentTime() + casNastupu);
+		if (mySim().isKrokovanie()) {
+			mySim().pauseSimulation();
+			mySim().pridajUdalostCoPozastavilaSimulaciu("Cestujúci " + cestujuci.getIdCestujuceho() + " nastupuje do vozidla: " + vozidlo.getIdVozidla());
+		}
+		hold(casNastupu, sprava);
 	}
 
 	//meta! userInfo="Process messages defined in code", id="0"
 	public void processDefault(MessageForm message) {
-		switch (message.code()) {
-		}
+		throw new RuntimeException("default vetva by nemala nastat");
 	}
 
 	//meta! userInfo="Generated code: do not modify", tag="begin"
@@ -55,4 +80,16 @@ public class ProcessNastupuCestujuceho extends Process {
 		return (AgentNastupuVystupu)super.myAgent();
 	}
 
+	private RNG<Double> getGeneratorNaZakaldeTypu(TYP_VOZIDLA typVozidla) {
+		if (typVozidla == TYP_VOZIDLA.MINIBUS) {
+			return _uniformContinuousRNG;
+		} else {
+			return _triangularRNG;
+		}
+	}
+
+	@Override
+	public SimulaciaDopravy mySim() {
+		return (SimulaciaDopravy) super.mySim();
+	}
 }
--- src/continualAssistants/ProcessVystupuCestujuceho.java
+++ src/continualAssistants/ProcessVystupuCestujuceho.java
@@ -1,12 +1,20 @@
 package continualAssistants;
 
+import Model.Cestujuci;
+import Model.Enumeracie.TYP_VOZIDLA;
+import Model.Vozidlo;
 import OSPABA.*;
+import OSPRNG.TriangularRNG;
 import simulation.*;
 import agents.*;
 import OSPABA.Process;
 
 //meta! id="224"
 public class ProcessVystupuCestujuceho extends Process {
+
+	private TriangularRNG _triangularRNG = new TriangularRNG(0.6, 1.2, 4.2, SimulaciaDopravy.GENERATOR_NASAD); // todo uncoment
+	//private TriangularRNG _triangularRNG = new TriangularRNG(60.0, 120.0, 420.0, SimulaciaDopravy.GENERATOR_NASAD); // TODO remove
+
 	public ProcessVystupuCestujuceho(int id, Simulation mySim, CommonAgent myAgent) {
 		super(id, mySim, myAgent);
 	}
@@ -19,16 +27,30 @@ public class ProcessVystupuCestujuceho extends Process {
 
 	//meta! sender="AgentNastupuVystupu", id="230", type="Notice"
 	public void processStart(MessageForm message) {
+		Sprava sprava = (Sprava) message;
+		Vozidlo vozidlo = sprava.getVozidlo();
+		Cestujuci cestujuci = sprava.getCestujuci();
+		double casVystupu = getCasVystupuNaZakladeTypuVozidla(vozidlo.getTypVozidla());
+		cestujuci.setCasZaciatkuVystupovania(mySim().currentTime());
+		cestujuci.setCasKoncaVystupovania(mySim().currentTime() + casVystupu);
+
+		if (mySim().isKrokovanie()) {
+			mySim().pauseSimulation();
+			mySim().pridajUdalostCoPozastavilaSimulaciu("Cestujúci " + cestujuci.getIdCestujuceho() + " vystupuje z vozidla: " + vozidlo.getIdVozidla());
+		}
+
+		sprava.setCode(Mc.koniecVystupu);
+		hold(casVystupu, sprava);
 	}
 
 	//meta! sender="AgentNastupuVystupu", id="235", type="Notice"
 	public void processKoniecVystupu(MessageForm message) {
+		assistantFinished(message);
 	}
 
 	//meta! userInfo="Process messages defined in code", id="0"
 	public void processDefault(MessageForm message) {
-		switch (message.code()) {
-		}
+		throw new RuntimeException("default vetva by nemala nastat");
 	}
 
 	//meta! userInfo="Generated code: do not modify", tag="begin"
@@ -53,6 +75,19 @@ public class ProcessVystupuCestujuceho extends Process {
 	@Override
 	public AgentNastupuVystupu myAgent() {
 		return (AgentNastupuVystupu)super.myAgent();
+	}
+
+	@Override
+	public SimulaciaDopravy mySim() {
+		return (SimulaciaDopravy) super.mySim();
+	}
+
+	private double getCasVystupuNaZakladeTypuVozidla(TYP_VOZIDLA typVozidla) {
+		if (typVozidla == TYP_VOZIDLA.MINIBUS) {
+			return 4.0;
+		} else {
+			return _triangularRNG.sample();
+		}
 	}
 
 }
