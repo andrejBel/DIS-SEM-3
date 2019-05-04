2d74c6d4ee1bb62bf78f71486860f9c370b30af4
Andrej Beliančin <3210ado@gmail.com>
2019-05-04 12:11:23 +0200

    Stanok prec, akcie nastupu vystupu prec, agent nastupuVystupuPrec, !!!oprava planovac prichodov zakaznikov!!!

--- src/continualAssistants/PlanovacPrichodovCestujucichNaZastavku.java
+++ src/continualAssistants/PlanovacPrichodovCestujucichNaZastavku.java
@@ -7,13 +7,13 @@ import simulation.*;
 import agents.*;
 
 //meta! id="136"
-public class PlanovacPrichodovZakaznikovNaZastavku extends Scheduler {
+public class PlanovacPrichodovCestujucichNaZastavku extends Scheduler {
 
 	ExponentialRNG _exponentialRNG;
 	ZastavkaOkolie _zastavkaOkolie;
 	private int _pocetVygenerovanychZakaznikov;
 
-	public PlanovacPrichodovZakaznikovNaZastavku(int id, Simulation mySim, CommonAgent myAgent, ZastavkaOkolie zastavkaOkolie) {
+	public PlanovacPrichodovCestujucichNaZastavku(int id, Simulation mySim, CommonAgent myAgent, ZastavkaOkolie zastavkaOkolie) {
 		super(id, mySim, myAgent);
 		this._zastavkaOkolie = zastavkaOkolie;
 		_exponentialRNG = new ExponentialRNG(zastavkaOkolie.getParameterExponencialnehoRozdelenia(), SimulaciaDopravy.GENERATOR_NASAD);
@@ -37,18 +37,31 @@ public class PlanovacPrichodovZakaznikovNaZastavku extends Scheduler {
 
 	//meta! sender="AgentOkolia", id="211", type="Notice"
 	public void processZacniGenerovat(MessageForm message) {
-		message.setCode(Mc.prichodZakaznikaNaZastavku);
+		message.setCode(Mc.prichodCestujucehoNaZastavku);
 		Sprava sprava = (Sprava) message;
 		sprava.setZastavkaKonfiguracie(_zastavkaOkolie.getZastavkaKonfiguracia());
 		hold(_exponentialRNG.sample(), sprava);
 	}
 
 	//meta! sender="AgentOkolia", id="139", type="Notice"
-	public void processPrichodZakaznikaNaZastavku(MessageForm message) {
+	public void processPrichodCestujucehoNaZastavku(MessageForm message) {
+		if (mySim().currentTime() < _zastavkaOkolie.getCasZaciatkuGenerovaniaPrichodovCestujucich()) {
+			throw new RuntimeException("Nespravny cas zaciatok");
+		}
+		if (mySim().currentTime() > _zastavkaOkolie.getCasKoncaGenerovaniaPrichodovCestujucich()) {
+			throw new RuntimeException("Nespravny cas koniec");
+		}
+		if (_pocetVygenerovanychZakaznikov > _zastavkaOkolie.getZastavkaKonfiguracia().getMaximalnyPocetCestujucich()) {
+			throw new RuntimeException("Prislo viac ludi");
+		}
+
 		_pocetVygenerovanychZakaznikov++;
 		if (mySim().currentTime() < _zastavkaOkolie.getCasKoncaGenerovaniaPrichodovCestujucich() && _pocetVygenerovanychZakaznikov < _zastavkaOkolie.getZastavkaKonfiguracia().getMaximalnyPocetCestujucich()) {
 			Sprava sprava = (Sprava) message.createCopy();
-			hold(_exponentialRNG.sample(), sprava);
+			double dalsiCas = _exponentialRNG.sample();
+			if ((mySim().currentTime() + dalsiCas) < _zastavkaOkolie.getCasKoncaGenerovaniaPrichodovCestujucich()) {
+				hold(dalsiCas, sprava);
+			}
 		}
 		assistantFinished(message);
 	}
@@ -62,8 +75,8 @@ public class PlanovacPrichodovZakaznikovNaZastavku extends Scheduler {
 	@Override
 	public void processMessage(MessageForm message) {
 		switch (message.code()) {
-			case Mc.prichodZakaznikaNaZastavku:
-				processPrichodZakaznikaNaZastavku(message);
+			case Mc.prichodCestujucehoNaZastavku:
+				processPrichodCestujucehoNaZastavku(message);
 				break;
 
 			case Mc.zacniGenerovat:
