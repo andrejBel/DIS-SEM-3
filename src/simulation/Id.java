6b52503e601792186a1266478c48bd911c8b8edb
Andrej Beliančin <3210ado@gmail.com>
2019-05-04 12:13:58 +0200

    Id a MC upravene a doplnene

--- src/simulation/Id.java
+++ src/simulation/Id.java
@@ -13,19 +13,16 @@ public class Id extends IdList {
 	public static final int managerModelu = 101;
 	public static final int managerOkolia = 103;
 	public static final int managerPrepravy = 105;
-	public static final int managerNastupuVystupu = 109;
 	public static final int managerPohybu = 107;
 	public static final int managerZastavok = 108;
 	public static final int asistentVyjazdu = 1004;
 	public static final int asistentPresunu = 1005;
-	public static final int processVystupuCestujuceho = 1011;
-	public static final int akciaPrichodZakaznika = 1012;
-	public static final int akciaPresunVozidloNaDalsiuZastavku = 1013;
-	public static final int akciaVystupCestujuceho = 1015;
-	public static final int akciaNastupCestujuceho = 1016;
-	public static final int processNastupuCestujuceho = 1010;
 	public static final int naplanujPresunVozidlaNaZastavku = 1020;
+	public static final int planovacPresunuVozidlaNaDalsiuZastavku = 1023;
+	public static final int procesNastupuCestujucich = 1021;
+	public static final int procesNastupCestujuceho = 1024;
+	public static final int procesVystupuCestujucich = 1022;
 
-	public static final int pociatocneIdPrePlanovacPrichodovZakaznikov_ = 10000;
+	public static final int pociatocneIdPrePlanovacPrichodovCestujucich_ = 10000;
 	//meta! tag="end"
 }
--- src/simulation/Mc.java
+++ src/simulation/Mc.java
@@ -5,17 +5,18 @@ package simulation;
 public class Mc extends IdList {
 	//meta! userInfo="Generated code: do not modify", tag="begin"
 	public static final int cestujuciNaZastavke = 1072;
-	public static final int prichodZakaznikaNaZastavku = 1051;
+	public static final int prichodCestujucehoNaZastavku = 1051;
 	public static final int init = 1004;
 	public static final int zacniGenerovat = 1062;
 	public static final int nastupCestujuceho = 1063;
 	public static final int vystupCestujuceho = 1064;
 	public static final int prichodVozidlaNaZastavku = 1040;
 	public static final int presunVozidlaNaDalsiuZastavku = 1044;
-	public static final int koniecVystupu = 1068;
-	public static final int koniecNastupu = 1070;
 	public static final int presunVozidlo = 1076;
 	public static final int prichodCestujucehoNaStadion = 1077;
+	public static final int cestujuciNastupil = 1081;
+	public static final int cestujuciVystupil = 1084;
+
 	//meta! tag="end"
 
 	// 1..1000 range reserved for user
